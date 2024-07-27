64182407a1eb73d9cb5206311527a64e8b3c77c0
Matheus Muniz <matheusmuniz215@gmail.com>
2024-07-27 15:57:12 -0300

    fix: email service crash

--- email-service/cmd/main.go
+++ email-service/cmd/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/MunizMat/microservices/email-service/internal/clients"
+	"github.com/MunizMat/microservices/email-service/internal/utils"
 )
 
 func main() {
+	utils.ParseEnvsOrPanic()
+
 	clients.Init()
 
 }
